cf3c01f2d4d5866825a3fbf65d46ee04d1cf0835
AllenZheng-05 <allenzhengadmissions@gmail.com>
2025-10-04 16:16:13 -0500

    changed the upload/download to be general and not hard coded to Allen's device

--- backend/testapp.js
+++ backend/testapp.js
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import { S3Client, GetObjectCommand, PutObjectCommand } from "@aws-sdk/client-s3";
 import fs from "fs";
+import path from "path";
 
 const config = {
   region: "us-east-2",
@@ -8,7 +9,9 @@ const config = {
 const client = new S3Client(config);
 
 async function uploadFileToS3(localFilePath, bucket, key, contentType = "application/octet-stream") {
-  const filestream = fs.createReadStream(localFilePath);
+  const absolutePath = path.isAbsolute(localFilePath) ? localFilePath : path.resolve(localFilePath);
+  const filestream = fs.createReadStream(absolutePath);
+  console.log(filestream)
   const input = {
     Body: filestream,
     Bucket: bucket,
@@ -17,11 +20,12 @@ async function uploadFileToS3(localFilePath, bucket, key, contentType = "applica
   };
   const uploadCommand = new PutObjectCommand(input);
   const uploadResponse = await client.send(uploadCommand);
-  console.log(`Uploaded ${localFilePath} to s3://${bucket}/${key}`);
+  console.log(`Uploaded ${absolutePath} to s3://${bucket}/${key}`);
   return uploadResponse;
 }
 
 async function downloadFileFromS3(bucket, key, localFilePath) {
+  const absolutePath = path.isAbsolute(localFilePath) ? localFilePath : path.resolve(localFilePath);
   const output = {
     Bucket: bucket,
     Key: key,
@@ -29,11 +33,19 @@ async function downloadFileFromS3(bucket, key, localFilePath) {
   const downloadCommand = new GetObjectCommand(output);
   const downloadResponse = await client.send(downloadCommand);
   const bytes = await downloadResponse.Body.transformToByteArray();
-  fs.writeFileSync(localFilePath, Buffer.from(bytes));
-  console.log(`Downloaded s3://${bucket}/${key} to ${localFilePath}`);
+  fs.writeFileSync(absolutePath, Buffer.from(bytes));
+  console.log(`Downloaded s3://${bucket}/${key} to ${absolutePath}`);
   return downloadResponse;
 }
 
 // Example usage:
-await uploadFileToS3("goat.jpg", "docbranchtestbucket", "goat.jpg", "image/png");
-await downloadFileFromS3("docbranchtestbucket", "goat.jpg", "downloadedgoat.jpg");
+import promptSync from 'prompt-sync';
+const prompt = promptSync();
+
+const uploadFileLocation = prompt('File path of uploaded file: ');
+const downloadLocation = prompt('Location of downloaded file: ');
+const s3Name = prompt('Name of file in s3: ');
+const fileType = "image/png";
+
+await uploadFileToS3(uploadFileLocation, "docbranchtestbucket", s3Name, fileType);
+await downloadFileFromS3("docbranchtestbucket", s3Name, downloadLocation);
