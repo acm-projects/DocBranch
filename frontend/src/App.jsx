ce9bf8940f3f50913949599593e7d40935317c52
kida-k <kidakhanooni1@gmail.com>
2025-10-13 10:53:29 -0500

    Tailwind CSS downloaded

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -28,6 +28,12 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <div className='bg-gray-100 min-h-screen p-8'>
+      <h1 className='text-3xl font-bold text-blue-600'>Tailwind CSS v4 is working!</h1>
+      <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4'>
+        Test Button
+      </button>
+    </div>
     </>
   )
 }
