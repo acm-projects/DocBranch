a57f34ec55ab81f5dff7f06b32c85d8ee7a42d41
AllenZheng-05 <allenzhengadmissions@gmail.com>
2025-10-15 02:01:28 -0500

    removed old resumeData.js file

--- backend/resume-generator/jsonToPDF.js
+++ backend/resume-generator/jsonToPDF.js
@@ -5,7 +5,7 @@ app.whenReady().then(() => {
   console.log('Electron started.');
   const path = require('path');
   // Get JSON file path from command line argument
-  const jsonFilePath = process.argv[2] ? path.resolve(process.argv[2]) : path.resolve(__dirname, 'resumeData.json');
+  const jsonFilePath = process.argv[2] ? path.resolve(process.argv[2]) : path.resolve(__dirname, 'jakes-resume.json');
   generateResume(jsonFilePath, "newjakes-resume.pdf");
 });
 
