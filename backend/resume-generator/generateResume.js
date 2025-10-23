f65b15ca542be8321df3a917ec57965b4a3e2105
AllenZheng-05 <allenzhengadmissions@gmail.com>
2025-10-22 19:00:34 -0500

    removed generateResume.js

--- backend/resume-generator/generateResume.js
+++
@@ -1,8 +0,0 @@
-
-const Resume = require('./Resume');
-
-function generateResume(jsonFilePath, outputFileName = 'resume.pdf') {
-  return Resume.fromFileToPDF(jsonFilePath, outputFileName);
-}
-
-module.exports = generateResume;
--- backend/resume-generator/jsonToPDF.js
+++ backend/resume-generator/jsonToPDF.js
@@ -1,12 +1,12 @@
 const { app } = require('electron');
-const generateResume = require('./generateResume');
+const Resume = require('./Resume');
 
 app.whenReady().then(() => {
   console.log('Electron started.');
   const path = require('path');
   // Get JSON file path from command line argument
   const jsonFilePath = process.argv[2] ? path.resolve(process.argv[2]) : path.resolve(__dirname, 'allenzheng-resume.json');
-  generateResume(jsonFilePath, "allenzheng-resume.pdf");
+  Resume.fromFileToPDF(jsonFilePath, "allenzheng-resume.pdf");
 });
 
 app.on('window-all-closed', () => app.quit());
